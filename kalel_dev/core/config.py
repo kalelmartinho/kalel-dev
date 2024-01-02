64d1cfabbd3692cb6628941f66a89f2ac88c3c52
Kalel L. Martinho <kalel@tutamail.com>
2024-01-01 21:50:53 -0300

    feat: Add settings

---
+++ kalel_dev/core/config.py
@@ -0,0 +1,17 @@
+from pydantic_settings import BaseSettings
+
+
+class Settings(BaseSettings):
+
+    cookie_exp: int = 60 * 60 * 24
+    token_key: str = "secret"
+
+    db_user: str = ""
+    db_password: str = ""
+    db_host: str = "localhost"
+    db_port: int = 27017
+    db_name: str = ""
+    db_url: str = f"mongodb://{db_host}:{db_port}"
+
+
+settings = Settings()
