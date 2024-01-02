1891fecfa4cbe6f37fb547d3ecabe39041d3bfed
Kalel L. Martinho <kalel@tutamail.com>
2024-01-02 11:04:35 -0300

    fix: Renamed fastapi var

--- kalel_dev/main.py
+++ kalel_dev/main.py
@@ -6,14 +6,14 @@ from kalel_dev.routes.router import get_api_router, get_bff_router
 
 def create_app():
     """Factory function for creating the FastAPI app"""
-    fast_api = FastAPI()
+    fastapi = FastAPI()
 
-    fast_api.include_router(get_bff_router(), tags=["bff"])
-    fast_api.include_router(get_api_router(), prefix="/api")
+    fastapi.include_router(get_bff_router(), tags=["bff"])
+    fastapi.include_router(get_api_router(), prefix="/api")
 
-    fast_api.mount("/static", static_files, name="static")
+    fastapi.mount("/static", static_files, name="static")
 
-    return fast_api
+    return fastapi
 
 
 app = create_app()
