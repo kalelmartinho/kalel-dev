3490946e0330424ba39d4093c1889397d95b99cb
Kalel L. Martinho <kalel@tutamail.com>
2024-01-01 21:16:31 -0300

    feat: Add function to create session cookie

--- kalel_dev/services/auth.py
+++ kalel_dev/services/auth.py
@@ -6,3 +6,13 @@ from kalel_dev.core.config import settings
 def create_temp_token(email: EmailStr) -> str:
     serializer = URLSafeTimedSerializer(settings.token_key, salt='temp-token')
     return str(serializer.dumps(email))
+
+
+async def create_session_cookie(response, email: EmailStr) -> str:
+    token = create_temp_token(email)
+    response.set_cookie(
+        key="Authorization",
+        value=token,
+        expires=settings.refresh_token_expire,
+    )
+    return response
