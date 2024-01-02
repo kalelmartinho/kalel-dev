3ce045aa659e0b13cdcd73e15f7d5c86945c1a45
Kalel L. Martinho <kalel@tutamail.com>
2024-01-02 11:01:04 -0300

    feat: Add function to config app

--- kalel_dev/main.py
+++ kalel_dev/main.py
@@ -1,8 +1,20 @@
 from fastapi import FastAPI
 
+from kalel_dev.core.front import static_files
+from kalel_dev.routes.router import get_api_router, get_bff_router
+
 
 def create_app():
     """Factory function for creating the FastAPI app"""
-    app = FastAPI()
+    fast_api = FastAPI()
 
-    return app
+    fast_api.include_router(get_bff_router(), tags=["bff"])
+    fast_api.include_router(get_api_router(), prefix="/api")
+
+    fast_api.mount("/static", static_files, name="static")
+
+    return fast_api
+
+
+app = create_app()
+
