002a31e28e11e2ad13ac01ab4d3cf02c94fa156f
Kalel L. Martinho <kalel@tutamail.com>
2024-01-01 21:21:38 -0300

    feat: Add function to verify temp token

--- kalel_dev/services/auth.py
+++ kalel_dev/services/auth.py
@@ -1,11 +1,22 @@
-from itsdangerous import URLSafeTimedSerializer
+from itsdangerous import BadSignature, SignatureExpired, URLSafeTimedSerializer
 from pydantic import EmailStr
 from kalel_dev.core.config import settings
 
+token_serializer = URLSafeTimedSerializer(settings.token_key, salt='temp-token')
+
 
 def create_temp_token(email: EmailStr) -> str:
-    serializer = URLSafeTimedSerializer(settings.token_key, salt='temp-token')
-    return str(serializer.dumps(email))
+    return str(token_serializer.dumps(email))
+
+
+def verify_temp_token(token: str, max_age: int) -> bool:
+    try:
+        token_serializer.loads(token, max_age=max_age)
+    except SignatureExpired:
+        return False
+    except BadSignature:
+        return False
+    return True
 
 
 async def create_session_cookie(response, email: EmailStr) -> str:
@@ -16,3 +27,5 @@ async def create_session_cookie(response, email: EmailStr) -> str:
         expires=settings.refresh_token_expire,
     )
     return response
+
+
