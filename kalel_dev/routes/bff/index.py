b1d06d0ea6388a6697649286a2bf83c5c14546de
Kalel L. Martinho <kalel@tutamail.com>
2024-01-02 15:39:56 -0300

    feat: Add function to generate base_context

--- kalel_dev/routes/bff/index.py
+++ kalel_dev/routes/bff/index.py
@@ -1,9 +1,12 @@
 from fastapi import APIRouter, Request
 from kalel_dev.core.front import templates
+from kalel_dev.core.context import create_context
+from kalel_dev.core.enums.page import Page
 
+page = Page.HOME
 router = APIRouter()
 
 
 @router.get("/")
 async def index(request: Request):
-    return templates.TemplateResponse("/main.html", {"request": request})
+    return templates.TemplateResponse("/main.html", create_context(request, page))
